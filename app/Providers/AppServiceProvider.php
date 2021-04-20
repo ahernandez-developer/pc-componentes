e07897b9f5b93573d2e575890d175e71b7783dab
ahernandez.developer <ahernandez.developer@gmail.com>
2021-04-19 19:09:22 -0600

    added https force schema

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        URL::forceScheme('https');
+        if(config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
     }
 }
